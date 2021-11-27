6b5545e5644635058eb198f56ba253643698e338
vlad1slavM <46625953+vlad1slavM@users.noreply.github.com>
2021-11-27 09:40:41 +0500

    work without Docker and test

--- main.py
+++ main.py
@@ -1,14 +1,34 @@
 from aiohttp import web
-"GET /convert?from=RUR&to=USD&amount=42"
+import redis
+"GET /convert?from=USD&to=RU&amount=42"
+
+
+bd = {"RUR": 1}
+r = redis.Redis()
 
 
 async def convert(request):
     param_from = request.rel_url.query['from']
     param_to = request.rel_url.query['to']
     param_amount = request.rel_url.query['amount']
-    result = f"from : {param_from} to : {param_to} amount : {param_amount}"
-    return web.Response(text=str(result))
+    tmp = bd[param_from] ** -1 * int(param_amount)
+    answer = tmp * bd[param_to]
+    return web.Response(text=str(answer))
+
+
+async def database(request):
+    param_merge = request.rel_url.query['merge']
+    if int(param_merge) == 0:
+        bd.clear()
+        r.flushdb()
+    else:
+        param_currency = request.rel_url.query['currency']
+        param_amount = request.rel_url.query['amount']
+        r.set(param_currency, 1 / int(param_amount))
+        bd[param_currency] = 1 / int(param_amount)
+        print(bd)
 
 app = web.Application()
 app.add_routes([web.get('/convert', convert)])
+app.add_routes([web.post("/database", database)])
 web.run_app(app)
