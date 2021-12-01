da33203814c53627cdcdc84bced180ba7b87574e
vlad1slavM <46625953+vlad1slavM@users.noreply.github.com>
2021-12-01 23:16:41 +0500

    add http responses

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,10 @@ EXPOSE 8080
 
 RUN pip3 install -r requirements.txt
 
+COPY tests.py .
+
 COPY main.py .
+
+RUN python -m unittest tests.py
 
 CMD ["python3", "main.py"]
--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 from aiohttp import web
 import aioredis
 
-
 bd = {"RUR": 1}
 
 
-async def main():
+async def is_digit(str):
+    try:
+        float(str[0])
+        return True
+    except ValueError:
+        return False
+
+
+async def convert_handler(value_from, value_to, param_amount):
+    if await is_digit(value_from) is False and await is_digit(value_to) is False:
+        return web.json_response({"status": 400, "response": "both values are not a digit"})
+    elif await is_digit(value_from) is False:
+        return web.json_response({"status": 400, "response": "value from it`s not digit"})
+    elif await is_digit(value_to) is False:
+        return web.json_response({"status": 400, "response": "value to it`s not digit"})
+    else:
+        tmp = float(value_from[0]) * float(param_amount)
+        answer = tmp * float(value_to[0]) ** -1
+        return answer
+
+
+async def start_server():
     app = web.Application()
     app.add_routes([web.get('/convert', convert)])
     app.add_routes([web.post("/database", database)])
@@ -20,8 +40,7 @@ async def convert(request):
     param_amount = request.rel_url.query['amount']
     value_from = await r.mget(param_from)
     value_to = await r.mget(param_to)
-    tmp = int(value_from[0]) * int(param_amount)
-    answer = tmp * int(value_to[0]) ** -1
+    answer = await convert_handler(value_from, value_to, param_amount)
     return web.Response(text=str(answer))
 
 
@@ -29,13 +48,15 @@ async def database(request):
     r = request.app['redis']
     param_merge = request.rel_url.query.get('merge')
     if param_merge == '0':
-        print(121)
         await r.flushdb()
-        return {"status": 200}
-    else:
+        return web.json_response({"status": 200})
+    elif param_merge == '1':
         data = await request.json()
         await r.mset(data)
+        return web.json_response({"status": 200})
+    else:
+        return web.json_response({'status': 400, "response": "unknown merge argument"})
 
 
 if __name__ == '__main__':
-    web.run_app(main())
+    web.run_app(start_server())
